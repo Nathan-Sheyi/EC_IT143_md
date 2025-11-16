a5602c7c1d5d0229ad4f9fefa78696de314e36e6
Nathan-Sheyi <145876826+Nathan-Sheyi@users.noreply.github.com>
2025-11-15 22:40:35 -0800

    Add files via upload
    Adding deliverable W3.4.

--- EC_IT143_W3.4_md.sql
+++ EC_IT143_W3.4_md.sql
@@ -15,14 +15,41 @@ NOTES:
 This is where I talk about what this script is, why I built it, and other stuff...
  
 ******************************************************************************************************************/
-
+USE AdventureWorks2022;
+GO
+SELECT COUNT(*) AS TotalEmployees
+FROM HumanResources.Employee;
+SELECT 
+    CAST(COUNT(*) AS FLOAT) / 
+    (SELECT COUNT(*) FROM HumanResources.Employee) * 100 AS HR_Percentage
+FROM HumanResources.EmployeeDepartmentHistory AS edh
+JOIN HumanResources.Department AS d
+    ON edh.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Human Resources';
 -- Q1: I would like to know more about the hum resource department, analyze the total number of employee?
 -- A1: The total number of employee is 290
 
 -- Q2: I would like to know the percentage of emplyments in the Human resource department, can you calculate that?
 -- A2: 2.06896551724138
+SELECT TOP 5 
+    a.City,
+    COUNT(*) AS NumberOfCustomers
+FROM Sales.Customer AS c
+JOIN Person.Address AS a
+    ON c.PersonID = a.AddressID
+GROUP BY a.City
+ORDER BY NumberOfCustomers DESC;
 
 -- Q3:I want to understand where most of our customers live. Based on customer and address data, which cities have the highest number of registered customers? Please list the top five.
+SELECT TOP 1
+    p.Name AS ProductName,
+    COUNT(*) AS NumberOfChanges
+FROM Production.ProductListPriceHistory plph
+JOIN Production.Product p
+    ON plph.ProductID = p.ProductID
+GROUP BY p.Name
+ORDER BY NumberOfChanges DESC;
+
 -- A3: London
 --Paris
 --Concord
@@ -30,13 +57,31 @@ This is where I talk about what this script is, why I built it, and other stuff.
 --Burlingame
 
 -- Q4: I would like the product price that has changed the most, could you please analyze that?
+SELECT 
+    p.Name,
+    SUM(sod.OrderQty) AS Qty,
+    p.ListPrice,
+    p.StandardCost,
+    SUM(sod.UnitPrice * sod.OrderQty) AS NetRevenue
+FROM Sales.SalesOrderDetail sod
+JOIN Sales.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID
+JOIN Production.Product p ON sod.ProductID = p.ProductID
+WHERE p.Name LIKE '%Bike%'
+  AND soh.OrderDate BETWEEN '2011-07-01' AND '2011-09-30'
+GROUP BY p.Name, p.ListPrice, p.StandardCost;
 -- A4: Sport-100 Helmet, Blue
 
--- Q4: I want to see all bike orders from Q3 2011, including how many were sold, their list price, standard cost, and an estimate of net revenue. Can you create a list showing this information?
--- A4: Mountain Bike Socks, L	32	9.50	3.3963	182.40
--- Mountain Bike Socks, M	301	9.50	3.3963	1644.45
 
 -- Q5: Our marketing team wants to identify top-performing salespeople. For each salesperson, calculate total sales for 2022. Then, rank them by performance and include their territory. (Tables: Sales.SalesOrderHeader, Sales.SalesPerson, Sales.SalesTerritory)
+SELECT 
+    sp.BusinessEntityID AS SalesPersonID,
+    sp.SalesYTD AS TotalSales2022,
+    st.Name AS Territory
+FROM Sales.SalesPerson sp
+JOIN Sales.SalesTerritory st
+    ON sp.TerritoryID = st.TerritoryID
+ORDER BY TotalSales2022 DESC;
+
 -- A5: 276	4251368.5497	Southwest
 --289	4116871.2277	United Kingdom
 --275	3763178.1787	Northeast
@@ -53,6 +98,16 @@ This is where I talk about what this script is, why I built it, and other stuff.
 --280	1352577.1325	Northwest
 
 -- Q6: Our leadership team is evaluating delivery performance. For orders shipped in 2012, can you compare the average shipping time by territory? Include total orders, average delay, and identify which territories performed the best and worst.
+SELECT 
+    st.Name AS Territory,
+    COUNT(*) AS TotalOrders,
+    AVG(DATEDIFF(DAY, soh.OrderDate, soh.ShipDate)) AS AvgShippingDays
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesTerritory st 
+    ON soh.TerritoryID = st.TerritoryID
+WHERE YEAR(soh.ShipDate) = 2012
+GROUP BY st.Name
+ORDER BY AvgShippingDays ASC;  
 -- A6: Australia	887	7
 --Central	116	7
 --Canada	429	7
@@ -63,10 +118,18 @@ This is where I talk about what this script is, why I built it, and other stuff.
 --Southeast	149	7
 
 -- Q7: Which AdventureWorks tables contain a column named ‘Color’? Use INFORMATION_SCHEMA views.
+SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'Color';
 -- A7: Production	Product	Color
 --Production	vProductModelCatalogDescription	Color
 
 --Q8: List all columns in AdventureWorks that allow NULL values and belong to the Production schema..
+SELECT TABLE_NAME AS TableName, COLUMN_NAME AS ColumnName
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE IS_NULLABLE = 'YES'
+  AND TABLE_SCHEMA = 'Production'
+ORDER BY TABLE_NAME, COLUMN_NAME;
 --A8: BillOfMaterials	EndDate
 --BillOfMaterials	ProductAssemblyID
 --Document	Document
